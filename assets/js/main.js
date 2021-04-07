b90e9a6b38278cc5889f931746e976e90abdfed0
John Cardenas <jhoncardenaspinto@gmail.com>
2021-04-06 22:25:20 -0500

    Project finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,20 +14,70 @@ const showMenu = (toggleId, navId) => {
 showMenu("nav-toggle", "nav-menu");
 
 /*=========== SWIPER JS ===========*/
+let galleryThumbs = new Swiper(".gallery-thumbs", {
+  spaceBetween: 0,
+  slidesPerView: 0,
+});
+
+let galleryTop = new Swiper(".gallery-top", {
+  effect: "fade",
+  loop: true,
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
 
 /*=========== POPUP ===========*/
-const btnOpenVideo = document.querySelectorAll(".islands__video-content");
-const islandsPopup = document.getElementById("popup");
+const btnOpenVideo = document.querySelectorAll(".top__video-content");
+const topPopup = document.getElementById("popup");
 
 function popup() {
-  islandsPopup.classList.add("show-popup");
+  topPopup.classList.add("show-popup");
 }
 
 btnOpenVideo.forEach((b) => b.addEventListener("click", popup));
 
 const btnCloseVideo = document.getElementById("popup-close");
 btnCloseVideo.addEventListener("click", () => {
-  islandsPopup.classList.remove("show-popup");
+  topPopup.classList.remove("show-popup");
 });
 
 /*=========== GSAP ANIMATION ===========*/
+const controlImg = document.querySelectorAll(".controls__img");
+
+function ScrollAnimation() {
+  gsap.from(".top__subtitle", {
+    opacity: 0,
+    duration: 0.2,
+    delay: 0.2,
+    y: -20,
+  });
+  gsap.from(".top__title", {
+    opacity: 0,
+    duration: 0.3,
+    delay: 0.3,
+    y: -20,
+  });
+  gsap.from(".top__description", {
+    opacity: 0,
+    duration: 0.4,
+    delay: 0.4,
+    y: -20,
+  });
+  gsap.from(".top__button", {
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.5,
+    y: -20,
+  });
+  gsap.from(".top__video/content", {
+    opacity: 0,
+    duration: 0.6,
+    delay: 0.6,
+    y: -20,
+  });
+
+  topPopup.classList.remove("show-popup");
+}
+
+controlImg.forEach((e) => e.addEventListener("click", ScrollAnimation));
